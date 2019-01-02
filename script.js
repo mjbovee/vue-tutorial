15595b0315716f2be0f7672b33d9a13b15fc54db
Michael Bovee <michael.j.bovee@gmail.com>
2019-01-01 17:11:53 -0700

    added in a couple "apps"

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ new Vue({
       message: 'Hello Vue.js!'
     }
 })
+
+var app2 = new Vue({
+    el: '#app2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleDateString()
+    }
+})
+
+var app3 = new Vue({
+    el: '#app3',
+    data: {
+        seen: true
+    }
+})
